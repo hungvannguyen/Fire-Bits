e675927e5698eaf2f55ce51dba9831de6ef96302
hungvannguyen <nguyenvanhung08012002@gmail.com>
2025-04-03 17:11:38 +0700

    Initialize React application with Vite setup and basic routing

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className="home">
+            <h1 >Welcome to Firebits!</h1>
+            <p className="paragraphL">Your one-stop solution for all your fire-related needs.</p>
+        </div>
+    );
+}
