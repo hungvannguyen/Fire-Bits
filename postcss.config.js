291662839ee9fb9ec0310160c4fa540e4fd2f5b3
hungvannguyen <nguyenvanhung08012002@gmail.com>
2025-04-03 17:26:44 +0700

    Remove PostCSS configuration file

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    plugins: [
-        require('postcss-mixins'),
-        require('postcss-simple-vars'),
-        require('postcss-nested'),
-        require('tailwindcss'),
-        require('autoprefixer'),
-    ],
-};
