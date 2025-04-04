2ba6462bced3f04dde42ffa62e6edf04d590445f
hungvannguyen <nguyenvanhung08012002@gmail.com>
2025-04-04 09:58:08 +0700

    Add Header component with Tailwind CSS styles and Prettier configuration

---
+++ src/partials/Header.jsx
@@ -0,0 +1,36 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="__container">
+        <div className="__logo">
+          <a href="/">Firebits</a>
+        </div>
+
+        <nav className="__nav">
+          <a href="/" className="navText active">
+            Home
+          </a>
+          <a href="/about" className="navText">
+            About
+          </a>
+          <a href="/contact" className="navText">
+            Services
+          </a>
+          <a href="/projects" className="navText">
+            Projects
+          </a>
+          <a href="/reviews" className="navText">
+            Reviews
+          </a>
+          <a href="/blog" className="navText">
+            Blog
+          </a>
+          <a href="/contact" className="navText">
+            Contact
+          </a>
+        </nav>
+        <div></div>
+      </div>
+    </header>
+  );
+}
