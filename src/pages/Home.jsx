15fe35736e387e941f6f8dbbe97f0276e4dc027b
SonKkc <soncb.xyz@gmail.com>
2025-04-04 15:10:44 +0700

    background, hero heading, update font

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,43 @@
+import loaImage from '../assets/img/loa.png';
+
 export default function Home() {
-    return (
-        <div className="home">
-            <h1 >Welcome to Firebits!</h1>
-            <p className="paragraphL">Your one-stop solution for all your fire-related needs.</p>
+    
+  return (
+    <div className="">
+      <div className="hero">
+        <div className="heading-content">
+          <div className="subheading">
+            <div className="__text">
+              <h1>Leading</h1>
+            </div>
+            <div className="bg-text"> 
+              <div className="__text">
+                <h1>Innovation</h1>
+              </div>
+            </div>
+            <div className="__text">
+              <h1>in</h1>
+            </div>
+            <div className="__text">
+              <h1>Digital</h1>
+            </div>
+            <div className="bg-icon">
+                <div className="__icon">
+                <img src={loaImage} alt="" className="" />
+                </div>
+            </div>
+            <div className="__text">
+              <h1>Solution.</h1>
+            </div>
+          </div>
+
+          <div className="rich-text">
+            <p className="paragraphS">
+            We are a strategic partner, working alongside businesses to create innovative solutions that help them achieve sustainable success and exceptional growth.
+            </p>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
