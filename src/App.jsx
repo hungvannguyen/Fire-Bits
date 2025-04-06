23dd4a903f10750da1e532453f47db65d75fcdcf
hungvannguyen <nguyenvanhung08012002@gmail.com>
2025-04-06 16:54:22 +0700

    Add About page and update routing; change document title to "Fire Bits"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <div className="my_container mt-[82px]">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
           </Routes>
         </div>
       </div>
---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+export default function About() {
+  return (
+    <div className="hero">
+      <div className="__image">
+        {/* <img src="https://picsum.photos/1200/1200" alt="About Us" /> */}
+      </div>
+      <div className="__content">
+        <h1 className="text-5xl font-bold">
+          Firebits - Powering Innovation, Building Trust
+        </h1>
+      </div>
+    </div>
+  );
+}
